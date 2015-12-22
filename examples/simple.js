c8daa86e69f9c2d0be273ae3eb3fbebfd8738102
Johannes Stein <johannesstein@freeze-dev.com>
2015-12-22 04:29:13 +0100

    Update example

--- examples/simple.js
+++ examples/simple.js
@@ -6,6 +6,8 @@ herakles.addRepository('tmp', 'test', 'github:nodegit/test');
 
 herakles.find('tmp/test').sync().then(function then() {
   console.log('Everything synced up.');
-}).catch(function error() {
+}).catch(function error(err) {
   console.log('Error while syncing.');
+  console.log(err);
+  console.log(err.stack);
 });
